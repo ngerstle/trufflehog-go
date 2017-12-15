07996109717e3f690c0e79c3f6ad7fa0d1889882
ngerstle@d598s-l-d <ngerstle@gmail.com>
2017-12-15 20:11:18 +0100

    need to fix slice/array math; check commits

--- trufflehog.go
+++ trufflehog.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
+
+type issue struct {
+	reason string
+	commit *object.Commit
+}
 
 func CheckIfError(err error, message string) {
 	if err != nil {
@@ -15,11 +21,17 @@ func CheckIfError(err error, message string) {
 }
 
 func main() {
-	CheckRepo("https://github.com/src-d/go-siva")
+	issues, err := CheckRepo("https://github.com/src-d/go-siva")
+	CheckIfError(err, "CheckRepo failed...")
+	printIssues(issues)
 }
 
-func CheckRepo(repourl string) {
-	fmt.Println("checking %s", repourl)
+func printIssues(issues []issue) error {
+	fmt.Println(issues)
+	return nil
+}
+func CheckRepo(repourl string) ([]issue, error) {
+	fmt.Println("checking ", repourl)
 
 	repo, err := git.Clone(memory.NewStorage(), nil, &git.CloneOptions{
 		URL: repourl,
@@ -33,9 +45,31 @@ func CheckRepo(repourl string) {
 	cIter, err := repo.Log(&git.LogOptions{From: ref.Hash()})
 	CheckIfError(err, "retreiving commit history failed")
 
+	issues := make([]issue, 0)
 	err = cIter.ForEach(func(c *object.Commit) error {
-		fmt.Println(c)
-		return nil
+		return checkCommit(c, issues)
 	})
+	fmt.Println(issues)
+	//err = cIter.ForEach(func(c *object.Commit) error {
+	//	fmt.Println(c)
+	//	return nil
+	//})
 	CheckIfError(err, "checking commits failed")
+	return nil, nil
+}
+
+func checkCommit(c *object.Commit, issues []issue) error {
+	commitissues := make([]issue, 0)
+	newissue := issue{"commit has issue: " + c.String(), c}
+	commitissues = append(commitissues, newissue)
+	issues = append(commitissues)
+	return errors.New("not implemented")
+}
+
+
+func checkEntropy(issues []issue) error {
+	return errors.New("not implemented")
+}
+func checkRegexes(issues []issue) error {
+	return errors.New("not implemented")
 }
